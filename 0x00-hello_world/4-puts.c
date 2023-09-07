52c6983d8bb85db9ed0d99229eb0521d3ff069bb
owuama1 <kelechiowuama24@gmail.com>
2023-09-07 05:54:28 -0700

    print a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - print a string
+*
+*Return: 0
+*/
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
