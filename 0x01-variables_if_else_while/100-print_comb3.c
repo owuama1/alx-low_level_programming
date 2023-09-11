d280c65cb30e2e9c39170e1a4ed3ec50e97b2200
owuama1 <kelechiowuama24@gmail.com>
2023-09-11 05:45:45 -0700

    print combinations of double digis with commas and spaces

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - print all combinations of double digits with comma and space
+* Return: 0
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		int j = 48;
+		while (j < 58)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == 56 && j == 57)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
