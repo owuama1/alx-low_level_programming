0c10899c6d0cf78ed7aa906cddcb081bd28866c6
owuama1 <kelechiowuama24@gmail.com>
2023-09-11 05:54:02 -0700

    print combinations of triple digits in ascending order

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+* main - print all combinations of triple digits with comma and space
+* Return: 0
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		int j = 48;
+
+		while (j < 58)
+		{
+			int k = 48;
+
+			while (k < 58)
+			{
+				if (j > i && k > j)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i == 55 && j == 56 && k == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
