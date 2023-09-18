c8c0dcb555d4ca741779fcbc06baf7de312859b6
owuama1 <kelechiowuama24@gmail.com>
2023-09-18 04:39:40 -0700

    include 6-abs.c file in this file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,19 @@
 #include "main.h"
 /**
+* _abs - output the absolute value of a number
+*@n: the number
+*Return: the absolute value
+*/
+int _abs(int n)
+{
+        if (n < 0)
+        {
+                return (-n);
+        }
+        else
+                return (n);
+}
+/**
 *print_last_digit - print the last digit of n
 *@n: a number
 *Return: the last digit
