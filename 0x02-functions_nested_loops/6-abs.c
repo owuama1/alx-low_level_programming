afa06a74b836a054f0b1d51bdeb47fa4047c7837
owuama1 <kelechiowuama24@gmail.com>
2023-09-18 03:45:51 -0700

    give the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _abs - output the absolute value of a number
+*@n: the number
+*Return: the absolute value
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+		return (n);
+}
