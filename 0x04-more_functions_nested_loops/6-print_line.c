34a3eff8b8eddeed8e475ac5ebd97073fd002214
owuama1 <kelechiowuama24@gmail.com>
2023-09-28 03:59:02 -0700

    print _ n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_line - print _ n times
+*@n: integer
+*Return: 
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');	
+}
