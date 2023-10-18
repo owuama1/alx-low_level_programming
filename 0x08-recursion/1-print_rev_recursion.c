d98bd217a63ebf1768de35a354160ed525240f81
owuama1 <kelechiowuama24@gmail.com>
2023-10-18 08:12:36 -0700

    check betty

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,6 +11,6 @@ void _print_rev_recursion(char *s)
 		return; /* Base case: Stop when the end of the string is reached*/
 	}
 
-	_print_rev_recursion(s + 1); /*Recursively call the function with next character*/
+	_print_rev_recursion(s + 1); /*Recursively call the function with next char*/
 	_putchar(*s); /* Print the current character after the recursive call*/
 }
