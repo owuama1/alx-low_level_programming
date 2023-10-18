d75149e547b3c126ea649a3473c49d31ad9def15
owuama1 <kelechiowuama24@gmail.com>
2023-10-18 09:09:47 -0700

    remove for loop

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,35 +1,34 @@
 #include "main.h"
 /**
+*is_prime_helper - help find a prime number
+*@n: integer
+*@divisor: a divisor
+*Return: the sqrt
+*/
+int is_prime_helper(int n, int divisor)
+{
+	if (n <= 1)
+	{
+		return (0); /* Numbers less than or equal to 1 are not prime*/
+	}
+
+	if (divisor == 1)
+	{
+		return (1); /* 1 is not prime, but we've checked all possible divisors*/
+	}
+
+	if (n % divisor == 0)
+	{
+		return (0); /* n is divisible by divisor, so it's not prime*/
+	}
+	return (is_prime_helper(n, divisor - 1)); /* Check the next divisor*/
+}
+/**
 *is_prime_number - checks if n is a prime number
 *@n: integer
 *Return: 1 if tru and 0 if false
 */
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
-	{
-		return (0); /* Return 0 for numbers <= to 1, as they are not prime*/
-	}
-
-	if (n <= 3)
-	{
-		return (1); /* Return 1 for 2 and 3, which are prime numbers*/
-	}
-
-	if (n % 2 == 0 || n % 3 == 0)
-	{
-		return (0); /* Return 0 for numbers divisible by 2 or 3 (excluding 2 and 3)*/
-	}
-
-	for (i = 5; i * i <= n; i += 6)
-	{
-		if (n % i == 0 || n % (i + 2) == 0)
-		{
-			return (0); /* Return 0 for numbers divisible by i or i + 2*/
-		}
-	}
-
-	return (1); /* If none of the above conditions are met, the number is prime*/
+	return (is_prime_helper(n, n - 1)); /*Start to check with biggest divisor*/
 }
