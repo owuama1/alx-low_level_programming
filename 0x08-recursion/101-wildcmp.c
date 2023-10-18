68b53015fa63c434278b34eb9f331ae5b159f1cd
owuama1 <kelechiowuama24@gmail.com>
2023-10-18 09:45:39 -0700

    check betty

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,22 +9,22 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 	{
-		return 1; /* Both strings are empty, considered identical*/
+		return (1); /* Both strings are empty, considered identical*/
 	}
 
 	if (*s2 == '*')
 	{
 		if (*(s2 + 1) == '\0')
 		{
- 			return 1; /*The rest of s2 is '*', considered identical*/
+			return (1); /*The rest of s2 is '*', considered identical*/
 		}
 
 		if (*s1 == '\0')
 		{
-			return wildcmp(s1, s2 + 1); /* Check if * in s2 represents the end of s1*/
+			return (wildcmp(s1, s2 + 1)); /* Check if * in s2 represents the end of s1*/
 		}
-
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1)); /* Check with * representing empty string or end of s1*/
+	/* Check with * representing empty string or end of s1*/
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
 
 	if (*s1 == '\0' || *s2 == '\0' || *s1 != *s2)
