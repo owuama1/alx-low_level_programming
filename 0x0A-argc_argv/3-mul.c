ac72edfb9834560b7cf24deb7244e566498a002b
owuama1 <kelechiowuama24@gmail.com>
2023-10-19 04:33:26 -0700

    declare and define the variables separately

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,15 +8,19 @@
 */
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]); /* Convert the first argument to an integer*/
-	int num2 = atoi(argv[2]); /* Convert the second argument to an integer*/
-	int result = num1 * num2; /* Multiply the two numbers*/
+	int num1;
+	int num2;
+	int result;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	num1 = atoi(argv[1]); /* Convert the first argument to an integer*/
+	num2 = atoi(argv[2]); /* Convert the second argument to an integer*/
+	result = num1 * num2; /* Multiply the two numbers*/
 
 	printf("%d\n", result); /* Print the result followed by a new line*/
 	return (0);
