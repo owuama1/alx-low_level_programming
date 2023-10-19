7918b37a612a1b26b2c32a583577ca0156e73296
owuama1 <kelechiowuama24@gmail.com>
2023-10-19 03:07:56 -0700

    print before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+*my_startup_function - executes before the main function
+*
+*Return: nothing
+*/
+void my_startup_function(void) __attribute__ ((constructor));
+
+void my_startup_function(void)
+{
+	printf("You're beat! and yet,
+	\you must allow,\nI bore my house upon my back!\n");
+}
