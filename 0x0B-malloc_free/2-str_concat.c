d5a8b28fae9ca453843e25ded64deb707c68cb1e
owuama1 <kelechiowuama24@gmail.com>
2023-10-19 07:34:08 -0700

    include the other functions in the source code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,84 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+*_strcat - concatenates two strings
+*@dest: destination
+*@src: source
+*Return: a pointer to the resulting string dest
+*/
+char *_strcat(char *dest, char *src)
+{
+		int dest_len = 0;
+		int src_len = 0;
+		int i;
+
+		if (dest == NULL || src == NULL)
+		{
+			return (NULL); /* Handle NULL input gracefully*/
+		}
+
+
+		/* Find the length of dest and src strings*/
+		while (dest[dest_len] != '\0')
+		{
+			dest_len++;
+		}
+
+		while (src[src_len] != '\0')
+		{
+			src_len++;
+		}
+
+		/* Append src to dest, overwriting the null terminator of dest*/
+		for (i = 0; i < src_len; i++)
+		{
+			dest[dest_len + i] = src[i];
+		}
+
+		/* Add a new null terminator at the end of the concatenated string*/
+		dest[dest_len + src_len] = '\0';
+
+		return (dest);
+}
+/**
+*_strlen - finds the length of the string
+*@s: a string
+*Return: the length
+*/
+int _strlen(char *s)
+{
+		int i;
+
+		for (i = 0; *s != '\0'; i++)
+			s++;
+		return (i);
+}
+/**
+*_strcpy - copies the string pointed to by src to dest
+*@src: source
+*@dest: destination
+*Return: the pointer to dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+		char *dest_start = dest;
+
+		if (src == NULL || dest == NULL)
+		{
+			return (NULL);
+		}
+		while (*src != '\0')
+		{
+			*dest = *src;
+			dest++;
+			src++;
+		}
+
+		*dest = '\0';
+
+		return (dest_start);
+}
+/**
 *str_concat - concatenates two strings.
 *@s1: string
 *@s2: string
