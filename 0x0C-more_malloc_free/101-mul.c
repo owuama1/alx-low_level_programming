cb3bd07e3a1ad74630c3c60755666c88d6c6d7ec
owuama1 <kelechiowuama24@gmail.com>
2023-10-23 02:54:22 -0700

    go back to previous commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -28,11 +28,9 @@ int is_digit(char *str)
 */
 int main(int argc, char *argv[])
 {
-	mpz_t num1, num2, result;
-
-	mpz_init(num1);
-	mpz_init(num2);
-	mpz_init(result);
+	unsigned long num1 = strtol(argv[1], NULL, 10);
+	unsigned long num2 = strtol(argv[2], NULL, 10);
+	unsigned long result = num1 * num2;
 
 	if (argc != 3)
 	{
@@ -40,20 +38,13 @@ int main(int argc, char *argv[])
 		return (98); /* Incorrect number of arguments*/
 	}
 
-	if (mpz_set_str(num1, argv[1], 10) == -1 ||
-		mpz_set_str(num2, argv[2], 10) == -1)
+	if (!is_digit(argv[1]) || !is_digit(argv[2]))
 	{
 		printf("Error\n");
-		return (98); /* Arguments are not valid numbers*/
+		return (98); /* Arguments are not composed of digits*/
 	}
 
-	mpz_mul(result, num1, num2);
-
-	gmp_printf("%Zd\n", result);
-
-	mpz_clear(num1);
-	mpz_clear(num2);
-	mpz_clear(result);
+	printf("%lu\n", result);
 
 	return (0);
 }
