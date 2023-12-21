3165b5599b5a0897a35ba8f0f5ef357a3663a141
owuama1 <kelechiowuama24@gmail.com>
2023-12-21 05:16:23 -0800

    betty for 0-hash, use djb2 algorithm

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,7 +5,7 @@
  * hash_table_create - Creates a hash table
  * @size: The size of the array
  *
- * Return: A pointer to the newly created hash table, or NULL if an error occurs
+ * Return: pointer to the newly created hash table, or NULL if an error occurs
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
