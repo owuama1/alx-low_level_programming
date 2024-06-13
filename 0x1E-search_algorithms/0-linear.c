bce3d365abd8563540341ca83376ae430ff3211c
owuama1 <kelechiowuama24@gmail.com>
2024-06-13 20:49:27 +0000

    betty error and Readme

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,31 +1,31 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Searches for a value in an array of integers using Linear search
- * @array: Pointer to the first element of the array to search in
+ * linear_search - Searches for value in an array of ints using Linear search
+ * @array: Pointer to first element of the array to search in
  * @size: The number of elements in the array
  * @value: The value to search for
  *
- * Return: The first index where value is located, or -1 if value is not present
- *         in the array or if array is NULL
+ * Return: first index where value is located, or -1 if value is not present
+ *				 in the array or if array is NULL
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+	size_t i;
 
-    if (array == NULL)
-    {
-        return (-1);
-    }
+	if (array == NULL)
+	{
+		return (-1);
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-        {
-            return (i);
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
 
-    return (-1);
+	return (-1);
 }
