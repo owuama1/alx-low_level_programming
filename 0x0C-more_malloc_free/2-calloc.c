fa37a86c5b8667b82ee4b2434a3981f22397a287
owuama1 <kelechiowuama24@gmail.com>
2024-06-19 19:51:25 +0000

    edit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,47 +1,31 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 /**
-*_memset - fills memory with constant byted
-*@s: memory area pointed to by s
-*@b: constant byte
-*@n: numbero of bytes
-*Return: a pointer to the memory area s
-*/
-char *_memset(char *s, char b, unsigned int n)
-{
-		char *dest = s;  /* Save the original pointer for returning*/
-
-		while (n > 0)
-		{
-			*s = b;
-			s++;
-			n--;
-		}
-
-		return (dest);
-}
-/**
-*_calloc - allocates memory for an array, using malloc.
-*@nmemb: integer
-*@size: size of array
-*Return:  pointer to the allocated memory.
-*/
+  * _calloc - allocates memory of an array using malloc.
+  * @nmemb: number of elements in array.
+  * @size: size of elements of array.
+  *
+  * Return: NULL is size or nmemb == 0.
+  * NULL if malloc fails.
+  * Pointer to memory allocated if successful.
+  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	size_t total_size = (size_t)nmemb * size;
-	void *ptr = malloc(total_size);
+	void *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
 	{
-		return (NULL); /* Return NULL for nmemb or size equal to 0*/
+		return (NULL);
 	}
 
-	if (ptr == NULL)
+	for (i = 0; i < (nmemb * size); i++)
 	{
-		return (NULL); /* Return NULL if memory allocation fails*/
+		*((char *)(p) + i) = 0;
 	}
 
-	_memset(ptr, 0, total_size); /* Initialize the allocated memory to zero*/
-
-	return (ptr);
+	return (p);
 }
