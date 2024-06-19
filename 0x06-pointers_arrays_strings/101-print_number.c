9ecf4b07456a13b822adb7016f2de3d6235c6657
owuama1 <kelechiowuama24@gmail.com>
2024-06-19 19:26:27 +0000

    make some changes

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,25 @@
 #include "main.h"
+
 /**
-*print_number - Recursive function to print an integer
-*@n: number
-*Return: nothing
-*/
+ * print_number - prints an integer;
+ * @n: integer to be printed;
+ */
+
 void print_number(int n)
 {
+	unsigned int n1;
+
 	if (n < 0)
 	{
-		_putchar('-');  /* Handle negative numbers*/
-		n = -n;
-	}
-
-	if (n / 10 != 0)
+		n1 = -n;
+		_putchar('-');
+	} else
 	{
-		print_number(n / 10);  /* Recursively print the digits*/
+		n1 = n;
 	}
 
-	_putchar(n % 10 + '0');  /* Print the last digit as a character*/
+	if (n1 / 10)
+		print_number(n1 / 10);
+
+	_putchar((n1 % 10) + '0');
 }
